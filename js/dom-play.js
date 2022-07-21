39cc273edfa6bc8b342a9b56bcff2a87c03f3b98
liamageo <Liaina.Mageo@seattlecolleges.edu>
2022-07-21 16:09:34 -0700

    changes made

---
+++ js/dom-play.js
@@ -0,0 +1,46 @@
+// alert("Hi There!"); <-- used to confirm function of page
+function myAlert(){
+    alert("I'm clicked!");
+}
+
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    // mySpan.addEventListener("click",myAlert);
+
+    mySpan.addEventListener("click",function(ev){
+        //alert("The current actor is "+ mySpan.dataset.actor);
+            highlightActor(mySpan.dataset.actor);
+        });
+
+}
+
+function highlightActor(actor){
+
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor,highlight
+            mySpan.style.backgroundColor='yelow';
+        }else{//other actor, background white
+            mySpan.style.backgroundColor='white';
+        }
+
+
+    }
+
+}
+
+function highlight(el){
+
+    if(el.style.backgroundColor == 'yelow'){
+        el.style.backgroundColor='white';
+    }else{
+        el.style.backgroundColor='yelow';
+    }
+
+}
+
+
+
+
